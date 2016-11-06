07bfca3295302600f333ef3ace6fed7507229bb2
Marina1303 <pupina3@mail.ru>
2016-11-06 18:47:00 +0700

    Add files via upload

--- Marina.sql
+++ Marina.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 03 2016 г., 15:36
+-- Время создания: Ноя 06 2016 г., 14:43
 -- Версия сервера: 5.5.50
 -- Версия PHP: 5.4.45
 
@@ -28,8 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `city` (
   `id` int(11) NOT NULL,
-  `name_city` varchar(200) NOT NULL,
-  `fligh_id` int(11) NOT NULL
+  `name_city` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -41,11 +40,9 @@ CREATE TABLE IF NOT EXISTS `city` (
 CREATE TABLE IF NOT EXISTS `flight` (
   `id` int(11) NOT NULL,
   `number_flight` int(11) NOT NULL,
-  `departure_city` varchar(100) NOT NULL,
-  `arrival_city` varchar(100) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `time` time NOT NULL,
-  `status` tinyint(4) NOT NULL,
+  `date_deperture` datetime NOT NULL,
+  `time_departure` datetime NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
   `departure_city_id` int(11) NOT NULL,
   `arrival_city_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -60,9 +57,9 @@ CREATE TABLE IF NOT EXISTS `ticket` (
   `id` int(11) NOT NULL,
   `first_name` varchar(200) NOT NULL,
   `last_name` varchar(200) NOT NULL,
-  `date_birth` datetime NOT NULL,
+  `date_birth` date NOT NULL,
   `flight_id` int(11) NOT NULL,
-  `status_ticket` tinyint(4) NOT NULL
+  `status_ticket` tinyint(4) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -74,8 +71,7 @@ CREATE TABLE IF NOT EXISTS `ticket` (
 --
 ALTER TABLE `city`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `name_city` (`name_city`),
-  ADD KEY `fligh_id` (`fligh_id`);
+  ADD KEY `name_city` (`name_city`);
 
 --
 -- Индексы таблицы `flight`
@@ -83,8 +79,6 @@ ALTER TABLE `city`
 ALTER TABLE `flight`
   ADD PRIMARY KEY (`id`),
   ADD KEY `number_flight` (`number_flight`),
-  ADD KEY `departure_city` (`departure_city`),
-  ADD KEY `arrival_city` (`arrival_city`),
   ADD KEY `departure_city_id` (`departure_city_id`),
   ADD KEY `arrival_city_id` (`arrival_city_id`);
 
@@ -119,17 +113,11 @@ ALTER TABLE `ticket`
 --
 
 --
--- Ограничения внешнего ключа таблицы `city`
---
-ALTER TABLE `city`
-  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`fligh_id`) REFERENCES `flight` (`id`);
-
---
 -- Ограничения внешнего ключа таблицы `flight`
 --
 ALTER TABLE `flight`
-  ADD CONSTRAINT `flight_ibfk_2` FOREIGN KEY (`arrival_city_id`) REFERENCES `city` (`id`),
-  ADD CONSTRAINT `flight_ibfk_1` FOREIGN KEY (`departure_city_id`) REFERENCES `city` (`id`);
+  ADD CONSTRAINT `flight_ibfk_1` FOREIGN KEY (`departure_city_id`) REFERENCES `city` (`id`),
+  ADD CONSTRAINT `flight_ibfk_2` FOREIGN KEY (`arrival_city_id`) REFERENCES `city` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `ticket`
